f21ad48afa1bfb88e1e8e5246915ca2db137a2c7
Jhan Jaider Alvarez Tovar <89151104+JJMater10@users.noreply.github.com>
2024-04-10 19:07:03 -0500

    Add files via upload
    Modificar variables

---
+++ database.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+import certifi
+
+MONGO_URI = 'mongodb+srv://jaider:1234@cluster0.773wf9g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+ca = certifi.where()
+
+def dbConnection():
+    try:
+        client = MongoClient(MONGO_URI, tlsCAFile=ca)
+        db = client["dbb_products_app"]
+    except ConnectionError:
+        print('Error de conexión con la bdd')
+    return db
