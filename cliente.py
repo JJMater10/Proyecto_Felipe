4a555ccf8665b87ac1fd11e4ef5fc4fa2f65fcf7
Jhon Harold Franco Valencia <89151037+SpiritHat@users.noreply.github.com>
2024-04-13 22:39:56 -0500

    Lineas comentadas en cliente.py y index.html

--- cliente.py
+++ cliente.py
@@ -1,12 +1,14 @@
 class Cliente:
     def __init__(self, nombre, apellido, edad):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
+        # Método de inicialización de la clase Cliente
+        self.nombre = nombre  # Establece el atributo 'nombre' del cliente
+        self.apellido = apellido  # Establece el atributo 'apellido' del cliente
+        self.edad = edad  # Establece el atributo 'edad' del cliente
 
     def toDBCollection(self):
-        return{
-            'nombre': self.nombre,
-            'apellido': self.apellido,
-            'edad': self.edad
+        # Método para convertir el objeto Cliente en un diccionario para almacenar en una base de datos
+        return {
+            'nombre': self.nombre,  # Añade el nombre del cliente al diccionario
+            'apellido': self.apellido,  # Añade el apellido del cliente al diccionario
+            'edad': self.edad  # Añade la edad del cliente al diccionario
         }
