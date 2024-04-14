66369dbe67da23a2ad20c64216e99659856f9ed5
Jhan Jaider Alvarez Tovar <89151104+JJMater10@users.noreply.github.com>
2024-04-13 22:37:45 -0500

    lineas de codigo comentadas en database
    se comenta las lineas de codigo explicando su función

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
-from pymongo import MongoClient
-import certifi
+from pymongo import MongoClient  # Importa la clase MongoClient del módulo pymongo
+import certifi  # Importa el módulo certifi
 
-MONGO_URI = 'mongodb+srv://jaider:1234@cluster0.773wf9g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-ca = certifi.where()
+MONGO_URI = 'mongodb+srv://jaider:1234@cluster0.773wf9g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'  # URI de conexión a MongoDB
+ca = certifi.where()  # Obtiene la ubicación del archivo de certificados CA
 
-def dbConnection():
-    try:
-        client = MongoClient(MONGO_URI, tlsCAFile=ca)
-        db = client["dbb_clientes_app"]
-    except ConnectionError:
-        print('Error de conexión con la bdd')
-    return db
+def dbConnection():  # Define la función para establecer la conexión a la base de datos
+    try:  # Inicia el bloque try para manejar excepciones
+        client = MongoClient(MONGO_URI, tlsCAFile=ca)  # Crea un cliente MongoClient con la URI y el archivo de certificados CA
+        db = client["dbb_clientes_app"]  # Accede a la base de datos "dbb_clientes_app"
+    except ConnectionError:  # Captura la excepción ConnectionError en caso de error de conexión
+        print('Error de conexión con la bdd')  # Imprime un mensaje de error
+    return db  # Retorna el objeto de la base de datos
+
